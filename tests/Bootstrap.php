e417c4e28bb64d2804699946fd6e4666f5fd4be2
Madhankarthik <madhan.r@advisantgroup.com>
2016-12-06 14:26:19 +0530

    Keys updated

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -3,6 +3,6 @@
 $base = realpath(dirname(__FILE__) . '/..');
 require "$base/src/Emma.php";
 
-define('ACCOUNT_ID', 123456);
-define('API_PUBLIC_KEY', 'XXXXXXXXXXXXXXXXXXXX');
-define('API_PRIVATE_KEY', 'XXXXXXXXXXXXXXXXXXXX');
+define('ACCOUNT_ID', 1781475);
+define('API_PUBLIC_KEY', 'ba6bae0b91962f673b16');
+define('API_PRIVATE_KEY', '938139343e64a7fb62df');
